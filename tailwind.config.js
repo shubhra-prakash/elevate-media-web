6898bfeec1a1ecc06dafcbd4ca8a56d067d1c9a2
shubhra-prakash <prakashshubhra9@gmail.com>
2026-05-11 22:11:08 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'serif': ['Playfair Display', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
